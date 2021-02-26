3aa0262c6249d4c08337c875074a013b1cefc680
Michael Wang <michael1999wang@gmail.com>
2021-02-25 19:45:36 -0500

    test

--- spotstitch/App.js
+++ spotstitch/App.js
@@ -1,40 +1,30 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import BackButton from './src/BackButton';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NativeRouter, Switch, Route } from "react-router-native";
 
-const Stack = createStackNavigator();
+import Home from "./Home"
+import Login from "./Login"
 
-export default function App() {
-  return (
-    <NavigationContainer>
-    <Stack.Navigator>
-      {/* <Stack.screen name="Home" component={HomeScreen} /> */}
-      <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-    </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NativeRouter>
+        <View style={styles.container}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </View>
+      </NativeRouter>
+    );
+  }
 }
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <Button
-      title="Log In"
-      onPress={() =>
-        navigation.navigate('Profile', { name: 'Jane' })
-      }
-    />
-  );
-};
-
-const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is the login screen.</Text>;
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ spotstitch/login_signup/s.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export default function Style() { 
+    return StyleSheet.create({
+        container: {
+            backgroundColor: 'red',
+        },
+    })
+};

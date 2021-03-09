0b250e2ff7fba2c39f04efced731eab79b944968
Michael Wang <michael1999wang@gmail.com>
2021-03-09 16:08:11 -0500

    Added draft of community page with dummy data

--- spotstitch/screens/Home.js
+++ spotstitch/screens/Home.js
@@ -13,11 +13,14 @@ export default function Home({ navigation }) {
         navigation.navigate("Signup")
     }
     
+    const goToCommunityList = () => {
+        navigation.navigate("CommunityList")
+    }
+    
     // The text things are for spaces, not sure of a better way to do it
     return(
         <View style={Styles.MiddleOfScreen}>
             
-            <Text style={Styles.Title}> Home Page </Text>
             <View style={Styles.logoContainer}>
                 <Image style={Styles.logo} source={require('../assets/logo.jpeg')}></Image>
             </View>
@@ -35,6 +38,14 @@ export default function Home({ navigation }) {
             <TouchableWithoutFeedback onPress={goToSignup}>
                 <View style={Styles.Button}>
                     <Text style={Styles.ButtonText}> SIGN UP </Text>
+                </View>
+            </TouchableWithoutFeedback>
+
+            <Text> </Text>
+
+            <TouchableWithoutFeedback onPress={goToCommunityList}>
+                <View style={Styles.Button}>
+                    <Text style={Styles.ButtonText}> COMMUNITY LIST </Text>
                 </View>
             </TouchableWithoutFeedback>
         </View>

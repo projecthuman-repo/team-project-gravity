1f14865ffa192502c96e5e49cb2a7df66115b22d
diannamcallister <dd.mcallister@hotmail.ca>
2021-03-14 22:08:15 -0400

    updated .env file for deployment

--- backend/database/db_config.js
+++ backend/database/db_config.js
@@ -2,6 +2,6 @@ module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
-    DB: "spotstitch",
+    DB: process.env.DB_DB,
     dialect: "mysql"
 };

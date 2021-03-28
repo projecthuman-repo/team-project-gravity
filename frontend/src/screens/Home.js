0d3b698214287da08a6b4f6d1622e3ddd92c2afc
diannamcallister <dd.mcallister@hotmail.ca>
2021-03-28 12:03:51 -0400

    now able to be directed to login or signup

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {View, Text, TouchableWithoutFeedback, Image, Alert, Platform} from "react-native";
 import Auth0 from 'react-native-auth0';
+import useUser from '../hooks/queries/useUser';
 
 import Styles from "../style/Style";
 
@@ -11,20 +12,21 @@ export default function Home({ navigation }) {
     const mobile_credentials = require('../auth0-configuration-mobile');
     const auth0 = new Auth0(mobile_credentials);
 
+    const user = useUser(userId);
+
     const goToCommunityList = () => {
         //TODO: update userId to be something like "guest" which indicates no one has signed in
         // then anywhere that Platform.OS is used, replace with check to see if userId === 'guest'
         navigation.navigate("CommunityList")
     }
     
-    const checkUserExists = async (subUserId) => {
-        const userId = subUserId.replace("auth0|", "");
-        setUserId(userId);
+    const checkUserExists = () => {
 
-        // call query - if a user is returned, then do:
-        navigation.navigate("CommunityList")
-        // otherwise (if a user is not returned, meaning a new user was created):
-        // navigation.navigate("Picture")
+        if (user) {
+            navigation.navigate("CommunityList")
+        } else {
+            navigation.navigate("Picture")
+        }
     }
 
     const getUserId = async () => {
@@ -49,7 +51,10 @@ export default function Home({ navigation }) {
             setAccessToken(credentials.accessToken);
 
             const subUserId = await getUserId();
-            await checkUserExists(subUserId);
+
+            setUserId(subUserId.replace("auth0|", ""));
+            
+            checkUserExists();
 
         } catch(err) {
             console.log(err);
@@ -59,13 +64,13 @@ export default function Home({ navigation }) {
     // The text things are for spaces, not sure of a better way to do it
     return(
         <View style={Styles.MiddleOfScreen}>
-            
+            {console.log(user)}
             <View style={Styles.logoContainer}>
                 <Image style={Styles.logo} source={require('../images/logo.jpeg')}></Image>
             </View>
 
             {Platform.OS === 'web' ? null :
-            <TouchableWithoutFeedback onPress={onLogin}>
+            <TouchableWithoutFeedback onPress={() => onLogin()}>
                 <View style={Styles.Button}>
                     <Text style={Styles.ButtonText}> LOGIN </Text>
                 </View>

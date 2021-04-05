afb5bd9a79f427ca0c4d855181f08205b53c541f
diannamcallister <dd.mcallister@hotmail.ca>
2021-04-05 14:59:39 -0400

    able to interact w backend for android

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache, createNetworkInterface } from '@apollo/client';
+import {Platform} from "react-native";
 import { createUploadLink } from 'apollo-upload-client'
 
 import Navigator from "./HomeStack"
 
-const link = createUploadLink({uri:'http://localhost:4000/graphql'})
-// const link = createUploadLink({uri:'http://10.0.2.2:4000/graphql'})
+let link = createUploadLink({uri:'http://localhost:4000/graphql'})
+if (Platform.OS === 'android') {
+  link = createUploadLink({uri:'http://10.0.2.2:4000/graphql'})
+}
 
 const client = new ApolloClient({
     link,

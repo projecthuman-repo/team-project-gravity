0fbf0b5ad6e110bb2dc769f351719d1b66bcd76b
diannamcallister <dd.mcallister@hotmail.ca>
2021-04-14 23:48:18 -0400

    trying env variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,24 @@ import { ApolloClient, ApolloProvider, InMemoryCache, createNetworkInterface } f
 import {Platform} from "react-native";
 import { createUploadLink } from 'apollo-upload-client'
 
+// require('dotenv').config();
+// import {API_URL} from '#env';
+// import Config from 'react-native-config';
+// console.log("CONFIG");
+// console.log(Config);
+// console.log(Config.API_URL);
+
+console.log(process.env['NODE_ENV']);
+console.log(process.env);
+if (process.env['NODE_ENV'] === 'development'){
+  console.log("IN IF");
+} else if (process.env['NODE_ENV'] === 'staging') {
+  console.log("IN STAGING");
+}
+
 import Navigator from "./HomeStack"
 
-let link = createUploadLink({uri:'http://localhost:4000/graphql'})
+let link = createUploadLink({uri:`http://localhost:4000/graphql`})
 if (Platform.OS === 'android') {
   link = createUploadLink({uri:'http://10.0.2.2:4000/graphql'})
 }
